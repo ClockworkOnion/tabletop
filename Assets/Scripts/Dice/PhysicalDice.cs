fe1f01094e7e308ed592e83510f4871e24ccc722
Lars <larshappel@gmail.com>
2024-02-08 15:42:26 +0900

    Rolling phys dice

--- Assets/Scripts/Dice/PhysicalDice.cs
+++ Assets/Scripts/Dice/PhysicalDice.cs
@@ -10,20 +10,24 @@ public class PhysicalDice : NetworkBehaviour
     DiceSide[] sides;
 
     private bool isRolling = false;
+    private bool isRollValid = false;
+    private Vector3 rollDirection;
     public int sideCount;
+    public Material castValidMaterial;
+    public Material castInvalidMaterial;
     private bool hovered = false;
     [SerializeField] private bool dragging = false;
     private CameraControl camera;
     private PlayerNetworkHandler player;
     public GameObject LineRenderPrefab;
+    private LineRenderer currentLine;
+    
 
-    Vector3 lastPosition = new();
-    Quaternion lastRotation = new();
     Rigidbody rigidbody;
 
     void Start()
     {
-        GetComponent<DoubleClickListener>().doubleClicked.AddListener(Roll);
+        //GetComponent<DoubleClickListener>().doubleClicked.AddListener(Roll);
         camera = Camera.main.GetComponent<CameraControl>();
         sides = GetComponentsInChildren<DiceSide>();
         rigidbody = GetComponent<Rigidbody>();
@@ -32,11 +36,8 @@ public class PhysicalDice : NetworkBehaviour
     void Update()
     {
         // Code for clicking and dragging right mouse button. For more controlled throwing of the dice.
-        // LineRenderer component is in use by the Outline (?) and is deactivated when not hovering
-        // Maybe make another object for the line renderer?
         if (hovered && Input.GetMouseButtonDown(1))
         {
-            Debug.Log("MouseDragOn");
             dragging = true;
             player = GetComponent<DoubleClickListener>().clickingPlayer;
         }
@@ -45,21 +46,55 @@ public class PhysicalDice : NetworkBehaviour
         {
             camera.cameraLocked = true;
 
-            if (!TryGetComponent(out LineRenderer line)) // Cancel if there's no line renderer
+            DebugText.GetInstance().DisplayText("Rolldirection: " + rollDirection + "\nMagnitude: " + (player.transform.position - transform.position).magnitude);
+            if (!currentLine)
             {
-                dragging = false;
-                camera.cameraLocked = false;
-                return;
+                LineRenderer line = Instantiate(LineRenderPrefab).GetComponent<LineRenderer>();
+                currentLine = line;
             }
 
-            DebugText.GetInstance().DisplayText("Dragging line to " + player.transform.position.ToString());
-            line.SetPosition(1, player.transform.position);
+            Vector3[] positions = new Vector3[5];
+            Vector3 dieToMouse = player.transform.position - transform.position;
+            float magni = dieToMouse.magnitude;
+            isRollValid = magni > 3;
+            currentLine.material = isRollValid ? castValidMaterial : castInvalidMaterial;
+            rollDirection = isRollValid ? dieToMouse : new Vector3(0, 0, 0);
+            currentLine.positionCount = 5;
+
+            positions[0] = transform.position;
+            positions[4] = player.transform.position;
+
+            // Create and set the positions to make an arc with the line renderer
+            dieToMouse = player.transform.position - transform.position;
+            magni = dieToMouse.magnitude * .25f;
+            dieToMouse.Scale(new Vector3(.25f, .25f, .25f));
+            dieToMouse.y += magni;
+            positions[1] = transform.position + dieToMouse;
+
+            dieToMouse = player.transform.position - transform.position;
+            magni = dieToMouse.magnitude * .35f;
+            dieToMouse.Scale(new Vector3(.5f, .5f, .5f));
+            dieToMouse.y += magni;
+            positions[2] = transform.position + dieToMouse;
+
+            dieToMouse = player.transform.position - transform.position;
+            magni = dieToMouse.magnitude * .25f;
+            dieToMouse.Scale(new Vector3(.75f, .75f, .75f));
+            dieToMouse.y += magni;
+            positions[3] = transform.position + dieToMouse;
+            
+            currentLine.SetPositions(positions);
+
         }
         else if (dragging && !Input.GetMouseButton(1))
         {
             dragging = false;
             camera.cameraLocked = false;
+            Destroy(currentLine.gameObject);
+            currentLine = null;
 
+            if (isRollValid)
+                Roll(player, rollDirection);
         }
 
         if (!isRolling)
@@ -81,25 +116,23 @@ public class PhysicalDice : NetworkBehaviour
     {
         List<FetchDiceSelector> diceListeners = GameObject.Find("FetchlistPanel").GetComponentsInChildren<FetchDiceSelector>().Cast<FetchDiceSelector>().ToList();
         diceListeners.ForEach((die) => die.ListenToRoll(sideCount, rollResult: result));
+        player.HandleChatMsgServerRpc(sideCount + " rolled a " + result);
     }
 
-    private void LateUpdate()
+    public void Roll(PlayerNetworkHandler roller, Vector3 rollDirect)
     {
-        lastPosition = transform.position;
-        lastRotation = transform.rotation;
-    }
-
-    public void Roll(PlayerNetworkHandler roller)
-    {
-        rollDieServerRpc(new Vector3(0, 0, 0));
+        rollDieServerRpc(rollDirect);
     }
 
     [ServerRpc(RequireOwnership = false)]
-    public void rollDieServerRpc(Vector3 newPos)
+    public void rollDieServerRpc(Vector3 direction)
     {
         Rigidbody rb = GetComponent<Rigidbody>();
-        rb.AddForce(new Vector3(200, 200, 200));
-        rb.AddTorque(new Vector3(100, 100, 100));
+        float forceFactor = 50f; // So the dice actually move
+        //rb.AddTorque(direction);
+        rb.AddTorque(new Vector3(direction.z*5, 0, direction.x*-5));
+        direction.Scale(new Vector3(forceFactor, forceFactor, forceFactor));
+        rb.AddForce(direction + new Vector3(0, direction.magnitude * 1.5f, 0));
         LeanTween.delayedCall(.5f, () => { isRolling = true; });
     }
 
